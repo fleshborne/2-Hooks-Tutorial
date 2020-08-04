c3a0c1555af20b67467b97c716fd5a76df8f592f
Fleshborne <blakeathompson94@gmail.com>
2020-08-04 16:10:37 -0400

    successfully inplemented react-redux, with help of middleware THUNK

--- redux-crash-course/src/App.js
+++ redux-crash-course/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+
+import store from './store';
+
+// const store = createStore(() => [], {}, applyMiddleware);
+
 function App() {
   const [scrollDepth, setScrollDepth] = useState(0);
 
@@ -17,14 +24,18 @@ function App() {
   });
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <PostForm />
-        <Posts />
-        <p> Wow you've scrolled this: {Math.round(scrollDepth)}, far!</p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <PostForm />
+          <Posts />
+          <footer>
+            Wow you've scrolled this: {Math.round(scrollDepth)}, far!
+          </footer>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
--- redux-crash-course/src/components/Posts.js
+++ redux-crash-course/src/components/Posts.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-    };
-  }
   componentWillMount() {
-    // console.log(123);
-    // ^ this will let us know that we've been mounted.
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
 
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     posts: [],
+  //   };
+  // }
+  // componentWillMount() {
+  //   // console.log(123);
+  //   // ^ this will let us know that we've been mounted.
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then((res) => res.json())
+  //     .then((data) => this.setState({ posts: data }));
+  // }
+
   render() {
-    const postItems = this.state.posts.map((post) => (
+    const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -31,4 +37,7 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = (state) => ({
+  posts: state.posts.items,
+});
+export default connect(mapStateToProps, { fetchPosts })(Posts);
