5bba422f69c75832b206a8056c7e22f67be4c581
Fleshborne <blakeathompson94@gmail.com>
2020-08-04 16:23:45 -0400

    added redux devtools enhancer

--- redux-crash-course/src/components/Posts.js
+++ redux-crash-course/src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postActions';
 
@@ -36,6 +37,11 @@ class Posts extends Component {
     );
   }
 }
+
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   posts: state.posts.items,
